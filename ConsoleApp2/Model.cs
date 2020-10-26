1207c0a50c89aee34266cb0377c5bf6a6968e165
ace-ld <54129812+ace-ld@users.noreply.github.com>
2020-10-26 16:56:20 +0300

    Add project files.

---
+++ ConsoleApp2/Model.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp2
+{
+    public class Model
+    {
+        // ISO Букв. код валюты
+        public string CharCode { get; set; }
+        // Название валюты
+        public string Name { get; set; }
+        // Значение
+        public decimal Value { get; set; }
+
+        // базовая валюта рубль РФ
+        public decimal Curse(decimal valute = 100) => valute / Value;
+
+        public decimal Converter(decimal sum, string valute) 
+        {
+            // [Сумма в евро]=100 * 100 * 1,73(курс евро к рублю) / 100 * 1,94(курс доллара к рублю)
+
+            if (CharCode == valute)
+            {
+                
+            }
+
+            decimal x = sum * sum * Curse();
+            decimal y = sum * Curse();
+            return _ = x / y;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Xml.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        private static readonly Dictionary<string, decimal> rate = new Dictionary<string, decimal>();
+        private static decimal singleRate;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сумму: ");
+            var number = decimal.Parse(Console.ReadLine());
+            Console.Write("Введите валюту: ");
+            var val = Console.ReadLine();
+
+            WebClient client = new WebClient();
+
+            var xml = client.DownloadString("http://www.cbr.ru/scripts/XML_daily.asp");
+            XDocument xdoc = XDocument.Parse(xml);
+
+            var el = xdoc.Element("ValCurs").Elements("Valute");
+
+            foreach (var i in el)
+            {
+                rate.Add(i.Element("CharCode").Value, 100 / decimal.Parse(i.Element("Value").Value));
+            }
+
+            foreach (var i in rate)
+                if (val == i.Key) singleRate = number * i.Value;
+
+            foreach (var i in rate)
+                if (val != i.Key) Console.WriteLine($"{i.Key} - {(number * number * i.Value) / singleRate}");
+        }
+    }
+}
